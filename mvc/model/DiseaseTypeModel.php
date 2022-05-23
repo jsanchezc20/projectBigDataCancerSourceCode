a0dc018e01065bed62b24c00f811be822fbcee35
Jaime Sánchez Cárcel <jsanchezc20@ilg.cat>
2022-05-23 21:08:08 +0200

    Fitxers generats en 2022-05-23 21:08:08

--- mvc/model/DiseaseTypeModel.php
+++ mvc/model/DiseaseTypeModel.php
@@ -2,10 +2,11 @@
 class DiseaseTypeModel {
 	### PROPIETATS ###
 	public	$disease_type_code = "",
-			$name = '';
+			$name = "",
+			$description  = "";
 
 	### MÈTODES ###
-	# SELECT » RETORNA TOTS ELS ACCESSOS
+	# SELECT » RETORNA TOTS ELS TIPUS DE CANCER
 	public static function getAll() : array {
 
 		$strSQL = "SELECT * FROM diseases_types;";
@@ -24,33 +25,33 @@ class DiseaseTypeModel {
 	# INSERT » CREA UN NOU ACCÉS
 	public function insert() {
 
-		$strSQL = " INSERT INTO diseases_types(disease_type_code, description)
-					VALUES	(?, ?);";
+		$strSQL = " INSERT INTO diseases_types(disease_type_code, name, description)
+					VALUES	(?, ?, ?);";
 
-		return DB::insert($strSQL, [$this->disease_type_code, $this->description]);
+		return DB::insert($strSQL, [$this->disease_type_code, $this->name, $this->description]);
 	}
 
 	# UPDATE » ACTUALITZA ACCÉS EXISTENT PER ID
 	public function update() {
 
 		$strSQL = " UPDATE diseases_types 
-					SET disease_type_code = ?, description = ?
-					WHERE access_id = ?;";
+					SET disease_type_code = ?, name = ?, description = ?
+					WHERE disease_type_code = ?;";
 		
-		return DB::update($strSQL, [$this->disease_type_code, $this->description, $this->access_id]);
+		return DB::update($strSQL, [$this->disease_type_code, $this->name, $this->description, $this->disease_type_code]);
 	}
 
 	# DELETE » ESBORRA ACCÉS EXISTENT PER ID
-	public static function delete(int $intId) {
+	public static function delete(int $strCode) {
 
 		$strSQL = "DELETE FROM diseases_types WHERE disease_type_code = ?;";
 
-		return DB::delete($strSQL, [$intId]);
+		return DB::delete($strSQL, [$strCode]);
 	}
 
 	# RETORNA LA INFORMACIÓ DE L'OBJECTE
 	public function __toString() : string {
-		return	"disease_type_code:		$this->disease_type_code<br>
-				 description:	$this->description<br>";
+		return	"disease_type_code:	$this->disease_type_code<br>
+				 description:		$this->description<br>";
 	}
 }
